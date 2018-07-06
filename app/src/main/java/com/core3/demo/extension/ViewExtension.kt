04c478fbd907c7d4cc21be9c53461cec81636a86
Aleksandra Mikusek <xamikusek@gmail.com>
2018-07-07 00:32:45 +0200

    [extension][view] add: methods for setting view visibility

---
+++ app/src/main/java/com/core3/demo/extension/ViewExtension.kt
@@ -0,0 +1,15 @@
+package com.core3.demo.extension
+
+import android.view.View
+
+fun View.makeGone() {
+    this.visibility = View.GONE
+}
+
+fun View.makeVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.makeInvisible() {
+    this.visibility = View.INVISIBLE
+}
