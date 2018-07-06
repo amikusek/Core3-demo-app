5c7d732a58437c7ba5899c5ab5142c5f26eae2c6
Aleksandra Mikusek <xamikusek@gmail.com>
2018-07-07 00:46:03 +0200

    [main] add: implementation

--- app/src/main/java/com/core3/demo/viper/main/MainContract.kt
+++ app/src/main/java/com/core3/demo/viper/main/MainContract.kt
@@ -1,16 +1,26 @@
 package com.core3.demo.viper.main
 
 import android.app.Activity
+import com.core3.demo.data.entity.Post
 
 import com.hannesdorfmann.mosby.mvp.MvpView
 import com.mateuszkoslacz.moviper.iface.interactor.ViperRxInteractor
 import com.mateuszkoslacz.moviper.iface.routing.ViperRxRouting
+import io.reactivex.Observable
 
 interface MainContract {
 
-    interface View : MvpView
+    interface View : MvpView {
+        fun setList(posts: List<Post>)
+        fun showList()
+        fun showLoading()
+        fun showEmptyView()
+        fun showError(throwable: Throwable)
+    }
 
-    interface Interactor : ViperRxInteractor
+    interface Interactor : ViperRxInteractor {
+        fun getPosts(): Observable<List<Post>>
+    }
 
     interface Routing : ViperRxRouting<Activity>
 }
--- app/src/main/java/com/core3/demo/viper/main/MainInteractor.kt
+++ app/src/main/java/com/core3/demo/viper/main/MainInteractor.kt
@@ -1,5 +1,10 @@
 package com.core3.demo.viper.main
 
+import com.core3.demo.data.sync.SyncableRepository
+import com.core3.demo.data.sync.query.remote.GetPostsQuery
 import com.mateuszkoslacz.moviper.base.interactor.BaseRxInteractor
 
-class MainInteractor : BaseRxInteractor(), MainContract.Interactor
+class MainInteractor : BaseRxInteractor(), MainContract.Interactor {
+
+    override fun getPosts() = SyncableRepository().query(GetPostsQuery())
+}
