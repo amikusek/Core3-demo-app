f60d88268e3fae36caedbb1d8c707250677cdc73
Aleksandra Mikusek <xamikusek@gmail.com>
2018-07-07 00:34:27 +0200

    [extension][view] add: throttling view clicks

--- app/src/main/java/com/core3/demo/extension/ViewExtension.kt
+++ app/src/main/java/com/core3/demo/extension/ViewExtension.kt
@@ -1,6 +1,10 @@
 package com.core3.demo.extension
 
 import android.view.View
+import com.jakewharton.rxbinding2.view.RxView
+
+fun View.throttleClicks() = RxView.clicks(this)
+        .throttleFirst(200, java.util.concurrent.TimeUnit.MILLISECONDS)
 
 fun View.makeGone() {
     this.visibility = View.GONE
